6bfba48b19ff8c1766dd41bb1cbdbf6e5425a7e8
Ivan Kardapoltsev <98235510+Pisex@users.noreply.github.com>
2025-01-07 03:59:42 +0500

    Delete build directory

--- build/vip/linux-x86_64/network_connection.proto.d
+++
@@ -1,3 +0,0 @@
-vip/linux-x86_64/network_connection.pb.cc \
-vip/linux-x86_64/network_connection.pb.h: /home/pisex/SkinChanger/hl2sdk-cs2/thirdparty/protobuf-3.21.8/src/google/protobuf/descriptor.proto\
- /home/pisex/SkinChanger/hl2sdk-cs2/common/network_connection.proto
